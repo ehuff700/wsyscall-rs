f7a463cca9f247efa544ddbd514c49f8fb8f24da
infamous <ehuff007@gmail.com>
2024-08-20 16:50:52 -0400

    Direct Syscalls (#1)
    * progress: direct syscalls
    
    * feat: working direct syscall
    
    * feat: framework for indirect syscalls

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,15 @@
 #![no_std]
-#![feature(str_from_raw_parts)]
+#![feature(str_from_raw_parts, stmt_expr_attributes)]
+
+use lazy_static::lazy_static;
+use spin::Mutex;
+use utils::cache::ModuleCache;
 extern crate alloc;
-mod direct;
-mod indirect;
+pub const NTDLL_NAME: &str = "ntdll.dll";
+
+pub mod syscall;
 pub mod utils;
+
+lazy_static! {
+    pub static ref SSN_CACHE: Mutex<ModuleCache> = Mutex::new(ModuleCache::new());
+}
---
+++ src/syscall.rs
@@ -0,0 +1,160 @@
+#![allow(non_snake_case, non_camel_case_types)]
+
+#[allow(unused)]
+macro_rules! syscall {
+    // Base case: no arguments (just SSN)
+    ($ssn:expr) => {{
+        let status: i32;
+        core::arch::asm!(
+            "mov r10, rcx",
+            "mov eax, {0:e}",
+            #[cfg(feature = "direct")]
+            "syscall",
+            in(reg) $ssn,
+            lateout("rax") status,
+            options(nostack)
+        );
+        status
+    }};
+    ($ssn:expr, $field1:expr) => {{
+        let status: i32;
+        core::arch::asm!(
+            "mov r10, rcx",
+            "mov eax, {0:e}",
+            #[cfg(feature = "direct")]
+            "syscall",
+            in(reg) $ssn,
+            in("rcx") $field1,
+            lateout("rax") status,
+            options(nostack)
+        );
+        status
+    }};
+    ($ssn:expr, $field1:expr, $field2:expr) => {{
+        let status: i32;
+        core::arch::asm!(
+            "mov r10, rcx",
+            "mov eax, {0:e}",
+            #[cfg(feature = "direct")]
+            "syscall",
+            in(reg) $ssn,
+            in("rcx") $field1,
+            in("rdx") $field2,
+            lateout("rax") status,
+            options(nostack)
+        );
+        status
+    }};
+    ($ssn:expr, $field1:expr, $field2:expr, $field3:expr) => {{
+        let status: i32;
+        core::arch::asm!(
+            "mov r10, rcx",
+            "mov eax, {0:e}",
+            #[cfg(feature = "direct")]
+            "syscall",
+            in(reg) $ssn,
+            in("rcx") $field1,
+            in("rdx") $field2,
+            in("r8") $field3,
+            lateout("rax") status,
+            options(nostack)
+        );
+        status
+    }};
+    ($ssn:ident, $field1:expr, $field2:expr, $field3:expr, $field4:expr) => {{
+        let status: i32;
+        #[cfg(feature = "indirect")]
+        let (ssn, syscall_address) = ($ssn.ssn, $ssn.syscall_address);
+        #[cfg(feature = "direct")]
+        let ssn = $ssn.ssn;
+        core::arch::asm!(
+            "mov r10, rcx",
+            "mov eax, {0:e}",
+            #[cfg(feature = "direct")]
+            "syscall",
+            in(reg) ssn,
+            in("rcx") $field1,
+            in("rdx") $field2,
+            in("r8") $field3,
+            in("r9") $field4,
+            lateout("rax") status,
+            options(nostack)
+        );
+        status
+    }};
+}
+
+#[macro_export]
+macro_rules! syscall_imp {
+    ($syscall:ident, ($($field_name:ident: $field_type:ty),*)) => {
+        #[allow(non_snake_case)]
+        pub unsafe fn $syscall($($field_name: $field_type),*) -> $crate::utils::wintypes::NTSTATUS {
+            let syscall = {
+                $crate::SSN_CACHE
+                    .lock()
+                    .get_or_insert($crate::NTDLL_NAME, stringify!($syscall))
+            };
+            syscall!(syscall, $($field_name),*)
+        }
+    };
+}
+
+#[cfg(test)]
+mod tests {
+    extern crate std;
+    use core::ffi::{c_char, c_ulong};
+    use std::{eprintln, println};
+
+    #[repr(C)]
+    #[derive(Debug)]
+    pub struct SYSTEM_BASIC_INFORMATION {
+        pub Reserved: c_ulong,
+        pub TimerResolution: c_ulong,
+        pub PageSize: c_ulong,
+        pub NumberOfPhysicalPages: c_ulong,
+        pub LowestPhysicalPageNumber: c_ulong,
+        pub HighestPhysicalPageNumber: c_ulong,
+        pub AllocationGranularity: c_ulong,
+        pub MinimumUserModeAddress: usize,
+        pub MaximumUserModeAddress: usize,
+        pub ActiveProcessorsAffinityMask: usize,
+        pub NumberOfProcessors: c_char,
+    }
+
+    #[allow(non_upper_case_globals)]
+    const SystemBasicInformation: u32 = 0; // Corresponds to the SYSTEM_INFORMATION_CLASS enum.
+
+    #[test]
+    fn test_syscall_imp() {
+        syscall_imp!(NtQuerySystemInformation, (
+            SystemInformationClass: u32,
+            SystemInformation: *mut core::ffi::c_void,
+            SystemInformationLength: u32,
+            ReturnLength: *mut u32
+        ));
+
+        unsafe {
+            let mut system_info = core::mem::zeroed::<SYSTEM_BASIC_INFORMATION>();
+
+            let status = NtQuerySystemInformation(
+                SystemBasicInformation,
+                &mut system_info as *mut _ as *mut _,
+                core::mem::size_of::<SYSTEM_BASIC_INFORMATION>() as u32,
+                core::ptr::null_mut(),
+            );
+
+            if status == 0 {
+                // STATUS_SUCCESS
+                println!("Number of Processors: {}", system_info.NumberOfProcessors);
+                println!("return: {:?}", system_info)
+            } else {
+                eprintln!(
+                    "NtQuerySystemInformation failed with status: 0x{:X}",
+                    status
+                );
+            }
+            assert_eq!(status, 0);
+            assert!(system_info.NumberOfProcessors > 0);
+        }
+    }
+}
